092cc532cf86847e540a0d1a66d76cc86cdece57
IvanioDaVinchi <vanyashaplyko@gmail.com>
2022-07-02 19:03:30 +0300

    worked with MPlatform class, create BP class and placed it on scene

--- Source/testProjectCPP/MPlatform.cpp
+++ Source/testProjectCPP/MPlatform.cpp
@@ -3,3 +3,44 @@
 
 #include "MPlatform.h"
 
+AMPlatform::AMPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	if (HasAuthority())
+	{
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+
+	GlobalStartLocation = GetActorLocation();
+	GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
+
+}
+
+void AMPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (HasAuthority())
+	{
+		FVector Location = GetActorLocation();
+		float JourneyLength = (GlobalTargetLocation - GlobalStartLocation).Size();
+		float JourneyTravelled = (Location  - GlobalStartLocation).Size();
+
+		if (JourneyTravelled >= JourneyLength)
+		{
+			FVector temp = GlobalStartLocation;
+			GlobalStartLocation = GlobalTargetLocation;
+			GlobalTargetLocation = temp;
+		}
+
+		FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+		Location += Speed * DeltaTime * Direction;
+		SetActorLocation(Location);
+	}
+}
--- Source/testProjectCPP/MPlatform.h
+++ Source/testProjectCPP/MPlatform.h
@@ -3,15 +3,28 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/StaticMeshComponent.h"
+#include "Engine/StaticMeshActor.h"
 #include "MPlatform.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class TESTPROJECTCPP_API UMPlatform : public UStaticMeshComponent
+class TESTPROJECTCPP_API AMPlatform : public AStaticMeshActor
 {
 	GENERATED_BODY()
-	
+public:
+	AMPlatform();
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float Speed = 20;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (MakeEditWidget = ture))
+		FVector TargetLocation;
+
+private:
+	FVector GlobalStartLocation;
+	FVector GlobalTargetLocation;
 };
